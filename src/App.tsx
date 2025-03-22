a51bccef2e4b16c40c1803332d7b32a7013eeaca
Jason O'Grady <96305763+JasonOG@users.noreply.github.com>
2025-03-23 08:42:23 +1000

    HashRouter implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
 // Styles
